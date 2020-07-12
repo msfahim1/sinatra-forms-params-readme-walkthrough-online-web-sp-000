8fd7624ebf141c16524b556cd5b1c17c4eff35ec
Michael Fahim <michael_fahim@yahoo.com>
2020-07-12 00:52:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,6 +12,6 @@ class App < Sinatra::Base
     :favorite_food => "Green Eggs and Ham"
   }
   post '/food' do
-    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
-    end
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}."
+  end
 end
