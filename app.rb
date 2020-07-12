8077c766a31650524d394bff6c2a7b7a6c060a80
Michael Fahim <michael_fahim@yahoo.com>
2020-07-12 00:44:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,8 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/food_form' do
-    erb :food_form
-  end
+  params.to_s  end
 
   # Add your post route and action below
   params = {
