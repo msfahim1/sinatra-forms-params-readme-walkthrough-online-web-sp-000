b918e0076b5461c6c088f7dfb0cb67b60d064e71
Michael Fahim <michael_fahim@yahoo.com>
2020-07-12 00:50:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,6 +10,8 @@ class App < Sinatra::Base
   params = {
     :name => "Sam",
     :favorite_food => "Green Eggs and Ham"
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+
   }
   post '/food' do
       params.to_s
